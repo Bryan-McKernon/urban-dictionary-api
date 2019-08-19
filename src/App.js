77cddb12c91ac16d033fa4121787edca783d6d01
Bryan McKernon <bryanmckernon@gmail.com>
2019-08-19 13:23:34 -0700

    commit 8/19/19

--- src/App.js
+++ src/App.js
@@ -1,26 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import WordCard from './WordCard.js';
 
 function App() {
+
+// state
+//--------------------------------------------------------------------------------------
+const [searchInput, setsearchInput] = useState();
+const [CardsInflated, setCardsInflated] = useState(false);
+//--------------------------------------------------------------------------------------
+
+/*
+// Prototye inheritance
+function x (y) {
+  this.a = "Lol";
+}
+x.prototype.b = "ye";
+const q = new x;
+console.log(q.b);
+*/
+
+/*
+  const inputTextChangeEvent = (event) => {
+    setsearchInput(event.target.value);
+  }
+
+  const data = [];
+  let cards = null;
+
+  const SearchEvent = () => {   
+    if (searchInput != null) {
+      const data = [];
+      const http = new XMLHttpRequest();
+      let url = "https://mashape-community-urban-dictionary.p.rapidapi.com/define?term=" + searchInput;
+      http.open("GET", url);
+      http.setRequestHeader("X-RapidAPI-Key", "f5b13fbecdmsh4d39ea459e16f58p10d3dajsnfb1a431ba454");
+      http.send();    
+      http.onload = () => {
+        let request = JSON.parse(http.responseText);
+        if (http.status === 200) {
+          for (let i=0; i<request.list.length; i++) {
+            data.push(request.list[i]);
+          }
+          //console.log(array[0]["author"]);      
+          console.log(data);
+          setCardsInflated(true);
+        }  else {
+          console.log("connection fail :(");
+        }  
+      }
+
+      http.onloadend = () => {
+          cards = (
+            <div>
+              Yo
+              {data}
+            </div>
+            /*
+              [1,2,3].map(x => 2 * x)
+              [ 2, 4, 6 ]
+            *//*
+          );
+        }
+    }
+  }*/
+
+
+  
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input onChange={inputTextChangeEvent}></input>
+      <button onClick={SearchEvent}>Search</button>
+      {CardsInflated === true ?  cards : null}  
     </div>
   );
-}
 
+
+}
 export default App;
---
+++ src/WordCard.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const WordCard = (props) => {
+    return (
+        <div>Hi</div>
+    )
+}
+export default WordCard;
